59093e24a606e83c8ebb21d28a340bb9d2e6d8e9
NGonzaless <nikki.gonzales@dice205.com>
2024-10-18 02:31:35 +0800

    implement basic mapView component

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,5 +1,15 @@
 import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import CustomMapView from "@/components/MapView";
 export default function Index() {
+  const { location: riderLocation, ...rider } = useSelector(
+    (state: any) => state.rider
+  );
+  const dispatch = useDispatch();
   return (
+    <View style={styles.generic}>
+      <CustomMapView location={riderLocation}>
+      </CustomMapView>
+    </View>
   );
 }
